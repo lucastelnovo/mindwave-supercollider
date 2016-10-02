50f25eb8af23863c669f5ac795ff67d62c029b04
stefaan himpe <stefaan.himpe@gmail.com>
2016-10-02 22:17:27 +0200

    update copyright notices

--- mindwave.py
+++ mindwave.py
@@ -1,19 +1,19 @@
-##################################################################################
-#      This file is part of mindwave-python.                                     #
-#                                                                                #
-#      mindwave-python is free software: you can redistribute it and/or modify   #
-#      it under the terms of the GNU General Public License as published by      #
-#      the Free Software Foundation, either version 3 of the License, or         #
-#      (at your option) any later version.                                       #
-#                                                                                #
-#      mindwave-python is distributed in the hope that it will be useful,        #
-#      but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
-#      GNU General Public License for more details.                              #
-#                                                                                #
-#      You should have received a copy of the GNU General Public License         #
-#      along with mindwave-python.  If not, see <http://www.gnu.org/licenses/>.  #
-##################################################################################
+#########################################################################################
+#      This file is part of mindwave-supercollider.                                     #
+#                                                                                       #
+#      mindwave-supercollider is free software: you can redistribute it and/or modify   #
+#      it under the terms of the GNU General Public License as published by             #
+#      the Free Software Foundation, either version 3 of the License, or                #
+#      (at your option) any later version.                                              #
+#                                                                                       #
+#      mindwave-supercollider is distributed in the hope that it will be useful,        #
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of                   #
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    #
+#      GNU General Public License for more details.                                     #
+#                                                                                       #
+#      You should have received a copy of the GNU General Public License                #
+#      along with mindwave-supercollider.  If not, see <http://www.gnu.org/licenses/>.  #
+#########################################################################################
 
 import select, serial, threading
 
